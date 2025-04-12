e1208d26b8cbeba5f7e20988548482a75175681f
xusanov10 <ahmadjonxusanovv>
2025-04-12 13:20:07 +0500

    kamunal tolov

---
+++ Bank_Program/xusanov.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Program
+{
+    class xusanov
+    {
+        public static void MakePaymet(int type, double sum)
+        {
+            switch (type)
+            {
+                case 1:
+
+                    Console.WriteLine($"kamunal to'lovlar");
+                    Console.WriteLine("1. Issiq suv");
+                    Console.WriteLine("2. Gaz");
+                    Console.WriteLine("3. Elektr energiya");
+
+                home:;
+                    int kamunal = Convert.ToInt32(Console.ReadLine());
+
+                    if (kamunal < 0 || kamunal > 4)
+                    {
+                        Console.WriteLine("Xato kiritdingiz, qaytadan urinib ko'ring");
+                        goto home;
+                    }
+
+                  if (sum > 20000)
+                  { 
+                    if (kamunal == 1)
+                    {
+                        Console.WriteLine($"Issiq suv to'lovi: {sum}");
+                    }
+                    else if (kamunal == 2)
+                    {
+                        Console.WriteLine($"Gaz to'lovi: {sum}");
+                    }
+                    else if (kamunal == 3)
+                    {
+                        Console.WriteLine($"Elektr energiya to'lovi: {sum}");
+                    }
+                  } else Console.WriteLine("Tolovni amalga oshirish uchun 20000 so'mdan koproq mablag' kiritng ")
+                        break;
+
+
+            }
+        }
+    }
+}
