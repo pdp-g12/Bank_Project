46e4a1f8df4e67d039260c669a7948dda94cf21b
MrJorjinio <jorjiniomr@gmail.com>
2025-04-12 22:36:11 +0500

    User Bank features updated, userPassword checking function updated, some codes shortened and some important variables created

---
+++ Bank_Program/UserPassword.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_Program
+{
+    class userPassword
+    {
+
+        public static bool CheckPassword(string userPassword, string userInputPassword)
+        {
+            if (userInputPassword == userPassword) return true;
+            else return false;
+        }
+    }
+}
