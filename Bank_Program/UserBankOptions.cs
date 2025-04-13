4f82440ab21656d35c9919bc7b7a95e4e020ac3c
MrJorjinio <jorjiniomr@gmail.com>
2025-04-13 13:23:00 +0500

    Added new bank feature smobile call pay, user cash output from bank and added new feature to user balance credit remaining

--- Bank_Program/UserBankOptions.cs
+++ Bank_Program/UserBankOptions.cs
@@ -15,6 +15,8 @@ namespace Bank_Program
             Console.WriteLine("Bank xizmatlari: ");
             Console.WriteLine("1. Kamunal to'lovlar");
             Console.WriteLine("2. Kredit to'lovlari");
+            Console.WriteLine("3. Naqd pul olish");
+            Console.WriteLine("4. Mobil aloqa to'lovi");
             Console.WriteLine("Birinchi xizmat uchun 1 - ni bosing");
             Console.WriteLine("Ikkinchi xizmat uchun 2 - ni bosing");
             userBankPref = Convert.ToInt32(Console.ReadLine());
@@ -127,11 +129,12 @@ namespace Bank_Program
                                 Console.WriteLine("Xato kiritdingiz, qaytadan urinib ko'ring");
                                 goto Home;
                             }
-                            else if (userInsurrancePref == 2) { 
+                            else if (userInsurrancePref == 2)
+                            {
                                 Console.WriteLine("To'lovingiz uchun rahmat salomat bo'ling");
                                 return;
                             }
-                            RepeatPay:
+                        RepeatPay:
                             Console.WriteLine("Yana kamunal tolov qilasizmi? ");
                             Console.WriteLine("1. Ha ");
                             Console.WriteLine("2. Yoq ");
@@ -163,7 +166,7 @@ namespace Bank_Program
                         Console.WriteLine($"kredit qoldig'i: {userCreditRemaining}");
                     RetryCreditPay:
                         Console.WriteLine("1. Kredit to'lash ");
-                        
+
                         int userPayCreditPref = Convert.ToInt32(Console.ReadLine());
 
                         if (userPayCreditPref == 1)
@@ -181,7 +184,121 @@ namespace Bank_Program
                         else Console.WriteLine("Xato son kiritdingiz"); goto RetryCreditPay;
                     }
                     else Console.WriteLine("Xato son kiritdingiz"); goto CreditHome;
+                case 3:
+                RetryCashPay:
+                    Console.WriteLine("1.Naqd pul olish");
 
+                    int userCashPref = Convert.ToInt32(Console.ReadLine());
+                    if (userCashPref <= 0 || userCashPref > 1)
+                    {
+                        Console.WriteLine("Xato kiritdingiz, qaytadan urinib ko'ring:");
+                        goto RetryCashPay;
+                    }
+                    if (userAccBank < 20000)
+                    {
+                        Console.WriteLine("Foydalanuvchi mablag'i kamida 20000 so'mni tashkil qilishi kerak");
+                        return;
+                    }
+                    if (userCashPref == 1)
+                    {
+                        Console.WriteLine("Naqd pul olish summasini kiriting:");
+                        double userCash = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine($"Naqd pul olindi: {userCash}");
+                        userAccBank -= userCash;
+                        goto Home;
+                    }
+                    break;
+                case 4:
+                MobileCallHome:
+                    Console.WriteLine("1.Mobil aloqa to'lovi");
+                    int mobileCall = Convert.ToInt32(Console.ReadLine());
+                    if (mobileCall <= 0 || mobileCall > 1)
+                    {
+                        Console.WriteLine("Xato kiritdingiz, qaytadan urinib ko'ring");
+                        goto MobileCallHome;
+                    }
+                    if (mobileCall == 1)
+                    {
+                    UserMobileCallPrefHome:
+                        Console.WriteLine("1. Ucell");
+                        Console.WriteLine("2. Beeline");
+                        Console.WriteLine("3. Uzmobile");
+                        int mobileType = Convert.ToInt32(Console.ReadLine());
+                        if (mobileType <= 0 || mobileType > 3)
+                        {
+                            Console.WriteLine("Xato kiritdingiz, qaytadan urinib ko'ring");
+                            goto UserMobileCallPrefHome;
+                        }
+                        else if (mobileType == 1)
+                        {
+                        UcellHome:
+                            Console.WriteLine("Ucell uchun to'lov summasini kiriting: ");
+                            double userUcellMobileInputPrice = Convert.ToDouble(Console.ReadLine());
+                            if (userAccBank >= 2000)
+                            {
+                                Console.WriteLine($"Ucell uchun to'lov qabul qilindi: {userUcellMobileInputPrice}");
+                                userAccBank -= userUcellMobileInputPrice;
+                            }
+                            else if (userAccBank < 2000)
+                            {
+                                Console.WriteLine("Foydalanuvchi mablag'i kamida 2000 so'mni tashkil qilishi kerak");
+                            }
+                            else if (userUcellMobileInputPrice < 2000)
+                            {
+                                Console.WriteLine("Yetarli Mablag' kiritnagansiz!");
+                                Console.WriteLine("Kamida 2000so'm mablag' to'lanishi kerak");
+                                goto UcellHome;
+                            }
+                        }
+                        else if (mobileType == 2)
+                        {
+                        BeelineHome:
+                            Console.WriteLine("Beeline uchun to'lov summasini kiriting: ");
+                            double userBeelineMobileInputPrice = Convert.ToDouble(Console.ReadLine());
+                            if (userAccBank >= 2000)
+                            {
+                                Console.WriteLine($"Beeline uchun to'lov qabul qilindi: {userBeelineMobileInputPrice}");
+                                userAccBank -= userBeelineMobileInputPrice;
+                            }
+                            else if (userAccBank < 2000)
+                            {
+                                Console.WriteLine("Foydalanuvchi mablag'i kamida 2000 so'mni tashkil qilishi kerak");
+                            }
+                            else if (userBeelineMobileInputPrice < 2000)
+                            {
+                                Console.WriteLine("Yetarli Mablag' kiritnagansiz!");
+                                Console.WriteLine("Kamida 2000so'm mablag' to'lanishi kerak");
+                                goto BeelineHome;
+                            }
+                        }
+                        else if (mobileType == 3)
+                        {
+                        UzMobileHome:
+                            Console.WriteLine("Uzmobile uchun to'lov summasini kiriting: ");
+                            double userUzMobileMobileInputPrice = Convert.ToDouble(Console.ReadLine());
+                            if (userAccBank >= 2000)
+                            {
+                                Console.WriteLine($"UzMobile uchun to'lov qabul qilindi: {userUzMobileMobileInputPrice}");
+                                userAccBank -= userUzMobileMobileInputPrice;
+                            }
+                            else if (userAccBank < 2000)
+                            {
+                                Console.WriteLine("Foydalanuvchi mablag'i kamida 2000 so'mni tashkil qilishi kerak");
+                            }
+                            else if (userUzMobileMobileInputPrice < 2000)
+                            {
+                                Console.WriteLine("Yetarli Mablag' kiritnagansiz!");
+                                Console.WriteLine("Kamida 2000so'm mablag' to'lanishi kerak");
+                                goto UzMobileHome;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Xato son kiritdingiz ");
+                            goto MobileCallHome;
+                        }
+                    }
+                    break;
             }
         }
     }
